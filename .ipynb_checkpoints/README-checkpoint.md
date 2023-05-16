d08e64848e63b248c7be666635be75fe87f23f5e
ujjawaltyagii <ujjawal2002tyagi@gmail.com>
2023-05-16 11:20:00 +0530

    initial

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,13 @@
+## MADE FOR CRUZ HACKS - BEST BEGGINER
+
+#### Devpost Submission: https://devpost.com/software/drink-wise
+
+##### What does it do?
+The inspiration for DrinkWise came from the devastating effects of drunk driving and the need to address this problem on college campuses. DrinkWise is a solution that combines both facial recognition and audio recognition technology to determine an individual's level of sobriety. This allows us to check if they are fit to drive a car. 
+
+##### Features
+
+DrinkWise is a web-app with two tests to detect sobriety:
+
+- First is a face test, we trained this fully from scratch on a dataset of both drunk and sober people
+- Second we analyzed spectrograms & used an algorithm called levenshtein distance to be able to detect whether a user is slurring
---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request
+import os
+import face_model
+import voice_model
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def camera():
+  return render_template('camera.html')
+
+
+@app.route('/upload_photo', methods=['POST'])
+def upload_photo():
+    photo = request.files['photo']
+    if photo:
+        filename = photo.filename
+        # Set the destination directory where the photo will be saved
+        destination = os.path.join(os.getcwd(), 'uploads', filename)
+        photo.save(destination)
+
+        val = face_model.check_intoxicated(photo.filename)
+        with open('val.txt', 'w') as f:
+         f.write(str(val))
+
+    return render_template('voice_model.html')
+
+# @app.route('/upload_video', methods=['POST'])
+# def upload_video():
+#     video = request.files['video']
+#     if video:
+#         filename = video.filename
+#         # Set the destination directory where the video will be saved
+#         destination = os.path.join(os.getcwd(), 'uploads', filename)
+#         video.save(destination)
+@app.route('/upload_video', methods=['POST'])
+def upload_video():
+    video = request.files['video']
+    video_path = os.path.join(os.getcwd(), video.filename)  # Get the absolute file path
+
+    video.save(video_path)
+
+    phrase = 'energetic happiness'
+
+    with open('phrase.txt', 'w') as f:
+         f.write(phrase)
+
+    voice_model.get_wav_file(video.filename)
+
+    val2 = voice_model.check_slurring('test.wav', phrase)
+
+    r = open('val.txt', 'r')
+    val = r.read()
+    val = float(val)
+    res = val+val2
+
+    print(res/2)
+
+    if res/2 >= 0.6:
+      return render_template('intoxicated.html')
+    else:
+      return render_template('not_intoxicated.html')
+
+app.run(host='0.0.0.0', port=18)
